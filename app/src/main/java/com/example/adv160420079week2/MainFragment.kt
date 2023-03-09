c0da93c5106e4c826f2393ff177e87f15b570c4e
Austin Lieandro <s160420079@student.ubaya.ac.id>
2023-03-09 07:04:23 +0700

    Week 2

---
+++ app/src/main/java/com/example/adv160420079week2/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.example.adv160420079week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.Navigation
+import org.w3c.dom.Text
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnStart = view.findViewById<Button>(R.id.btnStart)
+        val txtName = view.findViewById<EditText>(R.id.txtName)
+        btnStart.setOnClickListener {
+            val playerName = txtName.text.toString()
+            val action = MainFragmentDirections.actionGameFragment(playerName)
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
